410f57258ae75fc6c699473404232e6ceb4ce48c
vturchanikov <113292747+vturchanikov@users.noreply.github.com>
2023-03-26 12:10:13 +0400

    Added controllers with views

---
+++ Controllers/GameController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TennisApp.Controllers;
+
+public class GameController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
