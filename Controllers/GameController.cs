eb7eac011f3943dfabac7b597786021208bc5ca6
vturchanikov <113292747+vturchanikov@users.noreply.github.com>
2023-03-29 17:48:23 +0400

    Updated Game Controller

--- Controllers/GameController.cs
+++ Controllers/GameController.cs
@@ -1,11 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TennisApp.Data;
 
 namespace TennisApp.Controllers;
 
 public class GameController : Controller
 {
+    private readonly ApplicationDbContext _context;
+
+    public GameController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
     public IActionResult Index()
     {
-        return View();
+        var games = _context.Games.ToList();
+
+        return View(games);
     }
 }
